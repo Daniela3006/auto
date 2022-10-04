9eed327b8a5115df8f3d20cc07e6f1232a05259e
Daniela3006 <91320359+Daniela3006@users.noreply.github.com>
2022-10-04 20:42:03 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -4,14 +4,19 @@
 где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
 $name = $_POST['user_name'];
+$sname = $_POST['user_sname'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
+$message = $_POST['message'];
 $token = "5558007855:AAGWTOWMFn3ntC98ZhzPiG8bVsI6jp4nYvw";
 $chat_id = "-871940879";
 $arr = array(
-  'Имя пользователя: ' => $name,
+  'Имя: ' => $name,
+  'Фамилия: ' => $sname,
   'Телефон: ' => $phone,
-  'Email' => $email
+  'Email: ' => $email,
+  'Сообщение: ' => $message,
+  
 );
 
 foreach($arr as $key => $value) {
